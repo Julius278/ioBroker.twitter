df443a91c2f09d1cc9b01493b3611cf4239a3bfb
Lauterbach Julius <LAUTEJ@de.softlab.net>
2020-06-04 00:36:03 +0200

    first commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"test_project adapter settings": {
+		"en": "Adapter settings for test_project",
+		"de": "Adaptereinstellungen für test_project",
+		"ru": "Настройки адаптера для test_project",
+		"pt": "Configurações do adaptador para test_project",
+		"nl": "Adapterinstellingen voor test_project",
+		"fr": "Paramètres d'adaptateur pour test_project",
+		"it": "Impostazioni dell'adattatore per test_project",
+		"es": "Ajustes del adaptador para test_project",
+		"pl": "Ustawienia adaptera dla test_project",
+		"zh-cn": "test_project的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
